25142d483c6ac47320d40b9643d3033d43d84a34
Shchybor <migelito785@gmail.com>
2022-10-11 23:16:01 +0100

    update

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,18 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+(() => {
+      document
+      .querySelector('.js-speaker-form')
+      .addEventListener('submit', e => {
+        e.preventDefault();
+
+        new FormData(e.currentTarget).forEach((value,name) =>
+        console.log(`${name}: ${value}`),
+        );
+      });
+    })();
